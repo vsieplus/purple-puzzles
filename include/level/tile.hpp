df8d6e5f54b2e30c8e53136106f5eafd8c49ecc7
Ryan <rdsie@ucsd.edu>
2020-05-23 18:12:36 -0700

    updated tiles

--- include/level/tile.hpp
+++ include/level/tile.hpp
@@ -1,16 +1,20 @@
 #ifndef TILE_HPP
 #define TILE_HPP
 
+#include <memory>
+
 #include <SDL.h>
 
 class Tile {
     private:
         int x, y, width, height;
+
         SDL_Rect tilesheetClip;      // where on tilesheet texture to render
         SDL_Rect renderArea;         // where on screen to render
 
-        SDL_Texture* tilesetTexture; // pointer to the right tilesheet texture
-
+        // pointer to the right tilesheet texture
+        std::shared_ptr<SDL_Texture> tilesetTexture; 
+        
     public:
         Tile(int mapX, int mapY, int tilesetX, int tilesetY, int width,
              int height, SDL_Texture* texture);
--- src/level/tile.cpp
+++ src/level/tile.cpp
@@ -1,14 +1,13 @@
 #include "Tile.hpp"
 
 Tile::Tile(int mapX, int mapY, int tilesetX, int tilesetY, int width, int height, 
-    SDL_Texture* texture) : x(mapX), y(mapY), width(width), height(height) {
+    std::shared_ptr<SDL_Texture> texture) : x(mapX), y(mapY), width(width), 
+    height(height), tilesetTexture(texture) {
 
     tilesheetClip = {tilesetX, tilesetY, width, height};
-    tilesetTexture = texture;
     renderArea = {mapX, mapY, width, height};
 }
 
 void Tile::render(SDL_Renderer* renderer) {
-
     SDL_RenderCopy(renderer, tilesetTexture, &tilesheetClip, &renderArea);
 }
